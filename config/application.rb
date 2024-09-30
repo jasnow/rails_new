e23659238d7587e10b63c428444545c8c101f927
Al Snow <jasnow@hotmail.com>
2024-09-29 20:38:54 -0400

    Upgraded gem(s): [actioncable,actionmailbox,actionmailer,actionpack,actiontext,actionview,activejob,activemodel,activerecord,activestorage,activesupport,benchmark,config.active_support.to_time_preserves_timezone,racc,rack,rails,railties,uri]

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module Rails6New
     # the framework and any gems in your application.
 
     config.active_support.cache_format_version = 7.0
+
+    config.active_support.to_time_preserves_timezone = :zone
   end
 end
